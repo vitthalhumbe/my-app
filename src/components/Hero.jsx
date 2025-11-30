3b2cf579bc6e2ba4010f1e0cf30d062e6e24048e
Vitthal Humbe <vitthal.3454@gmail.com>
2025-12-01 01:11:44 +0530

    Started coding in Next JS

---
+++ src/components/Hero.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import "./hero.css";
+import { ArrowRight } from "lucide-react";
+
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <div className="text">
+
+                <h1 className="hero-text">Vitthal Humbe</h1>
+            </div>
+            <div className="content">
+                <div className="left">
+
+                    <div className="location-placer">
+                        <div className="circle"></div>
+                        <p className="location">Kolhapur, MH, India</p>
+                    </div>
+                    <div className="profession">
+                        <p>AI/ML Engineer and <br/>Full Stack Developer</p>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="about">
+                        Hi,  I am Vitthal Humbe - an AI/ML Engineer and Full-Stack Web and App developer passionate about building real-world application.
+                    </div>
+                    <div className="resume-button">
+                        <a href="/Vitthal_Humbe_Resume.pdf" download>
+                            <button className="btn-resume">
+                                Resume <ArrowRight size={20} />
+                            </button>
+                            
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
